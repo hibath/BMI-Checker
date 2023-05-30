ea13f723102fa5c88b6a3c82d6428e893f3f9d1e
hibaagha <Diadora123>
2023-05-30 10:32:06 +0200

    Initial Commit

---
+++ BMI-Checker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Checker
+//
+//  Created by Hiba Agha on 30.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
